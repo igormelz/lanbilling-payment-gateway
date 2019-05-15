75061db295440627e8418274e3113bdf4a9397dc
igormelz <igorm@openfs.ru>
2019-05-15 11:45:57 +0300

    add logging

--- src/main/java/org/openfs/lanbilling/UndertowWebServerCustomizer.java
+++ src/main/java/org/openfs/lanbilling/UndertowWebServerCustomizer.java
@@ -1,5 +1,7 @@
 package org.openfs.lanbilling;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.embedded.undertow.UndertowServletWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
@@ -12,15 +14,19 @@ import io.undertow.server.handlers.RequestLimitingHandler;
 @Component
 public class UndertowWebServerCustomizer implements WebServerFactoryCustomizer<UndertowServletWebServerFactory> {
 	
+	private static final Logger LOG = LoggerFactory.getLogger(UndertowWebServerCustomizer.class);
+	
 	@Value("${server.rateLimit:3}")
 	private int rateLimit;
 	
 	@Override
 	public void customize(UndertowServletWebServerFactory factory) {
+		LOG.info("Starting customize Undertow");
 		factory.addDeploymentInfoCustomizers(deploymentInfo -> deploymentInfo.addInitialHandlerChainWrapper(new HandlerWrapper() {
 
 			@Override
 			public HttpHandler wrap(HttpHandler handler) {
+				LOG.info("Undertow set rateLimit:{}", rateLimit);
 				return new RequestLimitingHandler(rateLimit, handler);
 			}
 			
