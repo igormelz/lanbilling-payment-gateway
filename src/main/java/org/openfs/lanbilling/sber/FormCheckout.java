0279be05c3800d32e29ceab32de8e5b34a69d883
igormelz <igorm@openfs.ru>
2019-05-16 21:04:35 +0300

    set empty body

--- src/main/java/org/openfs/lanbilling/sber/FormCheckout.java
+++ src/main/java/org/openfs/lanbilling/sber/FormCheckout.java
@@ -23,6 +23,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
+import io.undertow.util.StatusCodes;
+
 @Component("formCheckout")
 @Configuration
 public class FormCheckout {
@@ -36,8 +38,8 @@ public class FormCheckout {
 	@EndpointInject(uri = "direct:sberbank")
 	ProducerTemplate producer;
 
-	@Value("${sber.userName}")
-	private String userName;
+	@Value("${sber.username}")
+	private String username;
 
 	@Value("${sber.password}")
 	private String password;
@@ -52,23 +54,24 @@ public class FormCheckout {
 	public void validate(Exchange exchange) throws Exception {
 		Message message = exchange.getIn();
 		message.removeHeaders("Camel*");
-
+		message.setBody("{}");
+		
 		// validate params
 		if (message.getHeader("uid") != null && !message.getHeader("uid", String.class).isEmpty()) {
-			LOG.info("Process verify UID:{}", message.getHeader("uid"));
+			LOG.info("Process verify uid:{}", message.getHeader("uid"));
 
 			final String account = message.getHeader("uid", String.class);
 
 			// validate format
 			if (!NUMBERS.matcher(account).matches()) {
 				LOG.error("UID:{} has bad format", account);
-				message.setHeader(Exchange.HTTP_RESPONSE_CODE, 404);
+				message.setHeader(Exchange.HTTP_RESPONSE_CODE, StatusCodes.NOT_FOUND);
 				return;
 			}
 
 			// connect to LB
 			if (!lbapi.connect()) {
-				message.setHeader(Exchange.HTTP_RESPONSE_CODE, 503);
+				message.setHeader(Exchange.HTTP_RESPONSE_CODE, StatusCodes.INTERNAL_SERVER_ERROR);
 				return;
 			}
 
@@ -77,47 +80,49 @@ public class FormCheckout {
 
 			// process error response
 			if (response.isSuccess()) {
-				LOG.info("UID:{} is success", account);
+				LOG.info("uid:{} is success", account);
+				message.setHeader(Exchange.HTTP_RESPONSE_CODE, StatusCodes.OK);
 				return;
 			}
 
-			LOG.warn("UID:{} not found", account);
-			message.setHeader(Exchange.HTTP_RESPONSE_CODE, 404);
+			LOG.warn("uid:{} not found", account);
+			message.setHeader(Exchange.HTTP_RESPONSE_CODE, StatusCodes.NOT_FOUND);
 			return;
 		}
 
 		// process request and set response code
 		LOG.error("Unknown params");
-		message.setHeader(Exchange.HTTP_RESPONSE_CODE, 404);
+		message.setHeader(Exchange.HTTP_RESPONSE_CODE, StatusCodes.NOT_ACCEPTABLE);
 	}
 
 	@Handler
 	public void checkout(Exchange exchange) {
 		Message message = exchange.getIn();
 		message.removeHeaders("Camel*");
-
+		message.setBody("{}");
+		
 		// validate parameters
 		if (message.getHeader("uid") == null || message.getHeader("amount") == null) {
-			LOG.error("Unknown checkout params");
-			message.setHeader(Exchange.HTTP_RESPONSE_CODE, 400);
+			LOG.error("Unknown checkout parameters");
+			message.setHeader(Exchange.HTTP_RESPONSE_CODE, StatusCodes.NOT_ACCEPTABLE);
 			return;
 		}
 
 		final String account = message.getHeader("uid", String.class);
 		final Double amount = message.getHeader("amount", Double.class);
-		LOG.info("Process checkout for UID:{} on amount:{}", account, amount);
+		LOG.info("Processing checkout uid:{}, amount:{}", account, amount);
 
 		// connect to LB
 		if (!lbapi.connect()) {
-			message.setHeader(Exchange.HTTP_RESPONSE_CODE, 503);
+			message.setHeader(Exchange.HTTP_RESPONSE_CODE, StatusCodes.INTERNAL_SERVER_ERROR);
 			return;
 		}
 
 		// verify account
 		ServiceResponse response = lbapi.getAccount(CodeExternType.AGRM_NUM, account);
 		if (!response.isSuccess() || response.getValue(account) == null) {
-			LOG.error("UID:{} not found", account);
-			message.setHeader(Exchange.HTTP_RESPONSE_CODE, 404);
+			LOG.warn("uid:{} not found", account);
+			message.setHeader(Exchange.HTTP_RESPONSE_CODE, StatusCodes.NOT_FOUND);
 			lbapi.disconnect();
 			return;
 		}
@@ -126,36 +131,30 @@ public class FormCheckout {
 		response = lbapi.insertPrePayment(response.getLong(account), amount);
 		lbapi.disconnect();
 		if (!response.isSuccess()) {
-			LOG.error(EMAIL_ALERT,"Insert PrePayment error for UID:{}", account);
-			message.setHeader(Exchange.HTTP_RESPONSE_CODE, 500);
+			LOG.error("Insert prepayment error for uid:{}", account);
+			message.setHeader(Exchange.HTTP_RESPONSE_CODE, StatusCodes.INTERNAL_SERVER_ERROR);
 			return;
 		}
 
 		final long orderNumber = response.getLong(LbSoapService.FIELD_ORDER_NUMBER);
-		LOG.info(EMAIL_ALERT,"Insert PrePayment orderNumber:{} on amount:{} for UID:{}", orderNumber, amount, account);
+		LOG.info("Create prepayment orderNumber:{}, amount:{}, uid:{}", orderNumber, amount, account);
 
-		// prep query
+		// Processing sberbank acquiring
 		StringBuilder sb = new StringBuilder();
-		sb.append("userName=").append(userName);
+		sb.append("userName=").append(username);
 		sb.append("&password=").append(password);
-		sb.append("&currency=").append("643");
-		sb.append("&language=ru");
-		sb.append("&pageView=DESKTOP");
-		sb.append("&sessionTimeoutSecs=300");
+		sb.append("&currency=643").append("&language=ru").append("&pageView=DESKTOP").append("&sessionTimeoutSecs=300");
 		sb.append("&amount=").append(amount.intValue() * 100);
 		sb.append("&returnUrl=").append(successUrl);
 		sb.append("&failUrl=").append(failUrl);
 		sb.append("&orderNumber=").append(orderNumber);
-		// sb.append("&clientId=").append(account);
-		// sb.append("&jsonParams={\"orderNumber\":").append(orderNumber).append(",\"description\":\"Provider
-		// service\"}");
-		LOG.info("Call Sber with query: {}", sb.toString());
+
 		try {
 			Object answer = producer.requestBodyAndHeader(null, Exchange.HTTP_QUERY,
 					URLEncoder.encode(sb.toString(), "UTF-8"));
 			if (answer == null) {
-				LOG.error(EMAIL_ALERT,"Call to Sber has no response");
-				message.setHeader(Exchange.HTTP_RESPONSE_CODE, 500);
+				LOG.error(EMAIL_ALERT, "Call to Sber has no response");
+				message.setHeader(Exchange.HTTP_RESPONSE_CODE, StatusCodes.INTERNAL_SERVER_ERROR);
 				return;
 			}
 			if (answer instanceof Map) {
@@ -163,26 +162,26 @@ public class FormCheckout {
 				Map<String, String> sberResponse = (Map<String, String>) answer;
 				// process if error
 				if (sberResponse.containsKey("errorCode")) {
-					LOG.error(EMAIL_ALERT,"Sber return error:{}", sberResponse.get("errorMessage"));
-					message.setHeader(Exchange.HTTP_RESPONSE_CODE, 500);
+					LOG.error(EMAIL_ALERT, "Sber return error:{}", sberResponse.get("errorMessage"));
+					message.setHeader(Exchange.HTTP_RESPONSE_CODE, StatusCodes.INTERNAL_SERVER_ERROR);
 					return;
 				}
+				// redirected to payment form
 				if (sberResponse.containsKey("formUrl")) {
 					LOG.info("Sber return success orderId:{}", sberResponse.get("orderId"));
-					LOG.info("Reroute client to url:{}", sberResponse.get("formUrl"));
-					message.setHeader(Exchange.HTTP_RESPONSE_CODE, 301);
+					LOG.info("Redirected client to url:{}", sberResponse.get("formUrl"));
+					message.setHeader(Exchange.HTTP_RESPONSE_CODE, StatusCodes.MOVED_PERMANENTLY);
 					message.setHeader("Location", sberResponse.get("formUrl"));
 				}
 			}
 		} catch (CamelExecutionException e) {
-			LOG.error(EMAIL_ALERT,"Call to Sber got exception:{}", e.getMessage());
-			message.setHeader(Exchange.HTTP_RESPONSE_CODE, 500);
+			LOG.error("Call to Sber got exception:{}", e.getMessage());
+			message.setHeader(Exchange.HTTP_RESPONSE_CODE, StatusCodes.INTERNAL_SERVER_ERROR);
 			return;
 		} catch (UnsupportedEncodingException e) {
-			LOG.error(EMAIL_ALERT,"Call to Sber got URL exception:{}", e.getMessage());
-			message.setHeader(Exchange.HTTP_RESPONSE_CODE, 500);
+			LOG.error("Call to Sber got URL exception:{}", e.getMessage());
+			message.setHeader(Exchange.HTTP_RESPONSE_CODE, StatusCodes.INTERNAL_SERVER_ERROR);
 			return;
 		}
-
 	}
 }
