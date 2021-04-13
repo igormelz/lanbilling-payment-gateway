6ab0212de41e602d1e2e8ae8ddaa97cdd29b26f2
igormelz <igorm@openfs.ru>
2021-04-13 15:04:25 +0300

    up-to-date running config

--- src/main/java/ru/openfs/lbpay/sberonline/SberOnlineRoute.java
+++ src/main/java/ru/openfs/lbpay/sberonline/SberOnlineRoute.java
@@ -12,6 +12,8 @@ import ru.openfs.lbpay.lbsoap.LbSoapService;
 
 import static org.apache.camel.builder.PredicateBuilder.and;
 
+import java.util.UUID;
+
 import org.apache.camel.LoggingLevel;
 
 @Component
@@ -22,7 +24,7 @@ public class SberOnlineRoute extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-
+        ExternalId keyGen = new ExternalId();
         SberOnlineResponseFactory response = new SberOnlineResponseFactory();
         Predicate validAction = and(header(PaymentGatewayConstants.ACTION).isNotNull(),
                 header(PaymentGatewayConstants.ACTION).in(PaymentGatewayConstants.ACTION_CHECK,
@@ -55,9 +57,28 @@ public class SberOnlineRoute extends RouteBuilder {
                         .validate(validAmount).validate(validPayId).validate(validPayDate)
                         .log("Process payment:${header.PAY_ID} agreement:${header.ACCOUNT} amount:${header.AMOUNT}")
                         .bean(lbapi,"processDirectPayment")
+                        // check success
+                        .filter(simple("${body.getCode} == 0"))
+                            .setHeader(PaymentGatewayConstants.ORDER_NUMBER, header(PaymentGatewayConstants.PAY_ID))
+                            .setHeader(PaymentGatewayConstants.SBER_ORDER_NUMBER, method(keyGen,"getId"))
+                            .setHeader(PaymentGatewayConstants.ORDER_AMOUNT, simple("${body.paymentInfo.amount}"))
+                            .setHeader(PaymentGatewayConstants.CUSTOMER_PHONE, simple("${body.paymentInfo.customerPhone}"))
+                            .setHeader(PaymentGatewayConstants.CUSTOMER_EMAIL, simple("${body.paymentInfo.customerEmail}"))
+                            .setHeader(PaymentGatewayConstants.RECEIPT_TYPE,constant("SALE"))
+                            // audit register receipt
+                            .bean("audit", "registerReceipt")
+                            // register receipt
+                            .bean("ofdReceipt", "register")
+                        .end()
                     .end()
                     // remove work headers
-                    .removeHeaders("(ACTION|ACCOUNT|PAY_ID|PAY_DATE|AMOUNT)");
+                    .removeHeaders("(ACTION|ACCOUNT|PAY_ID|PAY_DATE|AMOUNT|phone|email|mdOrder|orderNumber|receiptType)");
     }
-
+    // gen externalId
+    class ExternalId {
+        public ExternalId() {}
+        public String getId() {
+            return UUID.randomUUID().toString();
+        }
+    }
 }
